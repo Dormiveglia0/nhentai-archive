928c0e88d0b197561dc3eef571f2a56ae61cbf87
root <root@SLC>
2026-06-12 18:50:40 +0800

    recover

--- frontend/src/components/Cover.tsx
+++
@@ -1,7 +0,0 @@
-export function Cover({ src, title, small }: { src: string; title: string; small?: boolean }) {
-  return (
-    <div className={`cover ${small ? 'small' : ''}`}>
-      <img src={src} alt={title} loading="lazy" />
-    </div>
-  );
-}
